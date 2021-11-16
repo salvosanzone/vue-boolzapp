a8a270d910732bea3335f8e91783d009024771c5
salvosanzone <salvosansone.ss@gmail.com>
2021-11-16 14:05:58 +0100

    inserimento click sui contatti

--- js/script.js
+++ js/script.js
@@ -5,6 +5,12 @@ Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visua
 
 */
 
+/*
+  Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+Click sul contatto mostra la conversazione del contatto cliccato (e seleziona il contatto come “attivo”)
+*/
+
 
 const app = new Vue({
 
@@ -16,27 +22,95 @@ const app = new Vue({
     //creo un array di oggetti
     contacts: [
       {
-        nome: 'Michele',
-        immagine: 'img/avatar_1.jpg'
+          name: 'Michele',
+          avatar: '_1',
+          visible: true,
+          messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'Hai portato a spasso il cane?',
+              status: 'sent'
+          },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Ricordati di dargli da mangiare',
+                  status: 'sent'
+              },
+              {
+                  date: '10/01/2020 16:15:22',
+                  message: 'Tutto fatto!',
+                  status: 'received'
+              }
+          ],
       },
       {
-        nome: 'Fabio',
-        immagine: 'img/avatar_2.jpg'
+          name: 'Fabio',
+          avatar: '_2',
+          visible: true,
+          messages: [{
+              date: '20/03/2020 16:30:00',
+              message: 'Ciao come stai?',
+              status: 'sent'
+          },
+              {
+                  date: '20/03/2020 16:30:55',
+                  message: 'Bene grazie! Stasera ci vediamo?',
+                  status: 'received'
+              },
+              {
+                  date: '20/03/2020 16:35:00',
+                  message: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                  status: 'received'
+              }
+          ],
       },
       {
-        nome: 'Samuele',
-        immagine: 'img/avatar_3.jpg'
+          name: 'Samuele',
+          avatar: '_3',
+          visible: true,
+          messages: [{
+              date: '28/03/2020 10:10:40',
+              message: 'La Marianna va in campagna',
+              status: 'received'
+          },
+              {
+                  date: '28/03/2020 10:20:10',
+                  message: 'Sicuro di non aver sbagliato chat?',
+                  status: 'sent'
+              },
+              {
+                  date: '28/03/2020 16:15:22',
+                  message: 'Ah scusa!',
+                  status: 'received'
+              }
+          ],
       },
       {
-        nome: 'Luisa',
-        immagine: 'img/avatar_4.jpg'
+          name: 'Luisa',
+          avatar: '_4',
+          visible: true,
+          messages: [{
+              date: '10/01/2020 15:30:55',
+              message: 'Lo sai che ha aperto una nuova pizzeria?',
+              status: 'sent'
+          },
+              {
+                  date: '10/01/2020 15:50:00',
+                  message: 'Si, ma preferirei andare al cinema',
+                  status: 'received'
+              }
+          ],
       },
-    ]
+  ],
 
+  activeContact: -1 
 
   },
 
   methods:{
+    //creo una funzione in cui do come parametro d'ingresso index
+    showMessages(index){
+      console.log('contatto numero:', index);
+    },
 
 
 
